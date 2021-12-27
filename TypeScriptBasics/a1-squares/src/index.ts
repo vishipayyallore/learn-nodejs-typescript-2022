f3c1a95d93b9de44ac346f2ff248c0861d8b73b0
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2021-12-28 00:50:01 +0530

    Few Changes

--- TypeScriptBasics/a1-squares/src/index.ts
+++ TypeScriptBasics/a1-squares/src/index.ts
@@ -1,10 +1,12 @@
-import { squares, squaresv1 } from "./squares";
+import { squares, squaresV1} from "./squares";
 
 const logMessage = (message: string): void => {
     console.log(message);
 }
 
 const values = [2, 3, 4, 5];
-logMessage(`square(${values}) = ${squares(values)}`);
+logMessage(`Square(${values}) = ${squares(values)}`);
+
+logMessage(`Square V1(${values}) = ${squaresV1(values)}`);
 
 logMessage('Hello World !! $$ ## %% ** &&');
--- TypeScriptBasics/a1-squares/src/squares.ts
+++ TypeScriptBasics/a1-squares/src/squares.ts
@@ -1,14 +1,12 @@
 
 function squares(values: number[]): number[] {
 
-    const results = values.map(x => x * x);
-
-    return results;
+    return values.map(x => x * x);
 }
 
-const squaresv1 = (values: number[]): number[] => {
+const squaresV1 = (values: number[]): number[] => {
 
     return values.map(x => x * x);
 }
 
-export { squares, squaresv1 };
+export { squares, squaresV1 };
