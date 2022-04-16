1513e4bc4cd474d7d48958c808616e659bc5d7c4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-04-16 13:21:45 +0530

    Renamed folder ./lib to ./dist

--- TypeScriptBasics/a2-sumfunction/src/index.ts
+++ TypeScriptBasics/a2-sumfunction/src/index.ts
@@ -1,4 +1,5 @@
 import { addNumbers } from './addition';
+import { makeTea } from './makeTea';
 
 const logMessage = (message: string): void => {
     console.log(message);
@@ -8,3 +9,10 @@ const values = [2, 4, 3, 5];
 logMessage(`${values[0]} + ${values[1]} = ${addNumbers(values[0], values[1])}`);
 
 logMessage(`${values[2]} + ${values[3]} = ${addNumbers(values[2], values[3])}`);
+
+
+// Make Tea
+logMessage(makeTea('hot'));
+logMessage(makeTea('cold'));
+// logMessage(makeTea('medium')); // Not Allowed
+
---
+++ TypeScriptBasics/a2-sumfunction/src/makeTea.ts
@@ -0,0 +1,7 @@
+
+const makeTea = (teaVariant: 'hot' | 'cold') => {
+
+    return `Here is your ${teaVariant} tea`;
+}
+
+export { makeTea };
