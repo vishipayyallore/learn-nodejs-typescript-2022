0ddb2cbcf6aed56fadc073a332f106a476f27ddf
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-06-15 13:53:13 +0530

    Update arraysdemo.ts

--- TypeScriptBasics/a7-arraysdemo/src/arraysdemo.ts
+++ TypeScriptBasics/a7-arraysdemo/src/arraysdemo.ts
@@ -2,6 +2,23 @@
 const carMakers: string[] = ['Ford', 'Chevrolet', 'Toyota'];
 console.log(carMakers);
 
+// Help with inference when extracting values
+const car1 = carMakers[0];
+const car2 = carMakers.pop();
+console.log(car1, car2);
+console.log(carMakers);
+
+// Prevent incompatible values
+// carMakers.push(100); // Illegal
+
+// Help with 'map'
+carMakers.map(
+    (car: string): string => {
+        console.log(car.toUpperCase());
+        return car.toUpperCase();
+    }
+);
+
 // const dates: Date[] = [];
 const dates = [new Date(), new Date()];
 console.log(dates);
@@ -13,3 +30,9 @@ const carsByMake: string[][] = [
     ['T150', 'T250', 'T350'],
 ];
 console.log(carsByMake);
+
+// Flexible types
+const importantDates: (Date | string)[] = [];
+importantDates.push('2030-10-10');
+importantDates.push(new Date());
+console.log(importantDates);
