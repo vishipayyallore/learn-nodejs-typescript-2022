8a94c00fcca511b2d61182116ab5e440f15faa9a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-06-15 13:35:49 +0530

    Initial Version

---
+++ TypeScriptBasics/a7-arraysdemo/src/arraysdemo.ts
@@ -0,0 +1,15 @@
+// const carMakers: string[] = [];
+const carMakers: string[] = ['Ford', 'Chevrolet', 'Toyota'];
+console.log(carMakers);
+
+// const dates: Date[] = [];
+const dates = [new Date(), new Date()];
+console.log(dates);
+
+// const carsByMake: string[][] = [];
+const carsByMake: string[][] = [
+    ['F150', 'F250', 'F350'],
+    ['C150', 'C250', 'C350'],
+    ['T150', 'T250', 'T350'],
+];
+console.log(carsByMake);
