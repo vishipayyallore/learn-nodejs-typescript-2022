71de56eeae062e37989959c33ab5bbfe23c7ff05
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-07-04 00:25:45 +0530

    Maps Demo Work

--- TypeScriptProjects/maps-tsdemo/src/index.ts
+++ TypeScriptProjects/maps-tsdemo/src/index.ts
@@ -1,5 +1,13 @@
+/// <reference types="@types/google.maps" />
+
+import { Company } from "./Company";
 import { User } from "./User";
 
 const user1 = new User();
 console.log(user1);
 console.log(user1.markerContent());
+
+const company1 = new Company()
+console.log(company1);
+
+console.log(google);
